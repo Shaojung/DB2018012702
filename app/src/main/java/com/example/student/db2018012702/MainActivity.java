3726bf71e5f657b9862ef1eda66c23efdefb7e62
Shaojung <shaojung@gmail.com>
2018-01-27 15:33:18 +0800

    get spinner

--- app/src/main/java/com/example/student/db2018012702/MainActivity.java
+++ app/src/main/java/com/example/student/db2018012702/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.student.db2018012702;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.textView);
         sp = findViewById(R.id.spinner);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String f[] = getResources().getStringArray(R.array.fruits);
+                tv.setText(f[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
     public void click1(View v)
     {
